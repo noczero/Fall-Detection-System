9a900e646612af253555d1ac7c31021577130e8e
noczero <zeroonetm@gmail.com>
2018-04-05 06:26:53 +0700

    LED Red DMP fix EMQTT bug

--- fallDetecionSystem.ino
+++ fallDetecionSystem.ino
@@ -1,15 +1,62 @@
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2012 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+/* This driver reads quaternion data from the MPU6060 and sends
+   Open Sound Control messages.
+
+  GY-521  NodeMCU
+  MPU6050 devkit 1.0
+  board   Lolin         Description
+  ======= ==========    ====================================================
+  VCC     VU (5V USB)   Not available on all boards so use 3.3V if needed.
+  GND     G             Ground
+  SCL     D1 (GPIO05)   I2C clock
+  SDA     D2 (GPIO04)   I2C data
+  XDA     not connected
+  XCL     not connected
+  AD0     not connected
+  INT     D8 (GPIO15)   Interrupt pin
+
+*/
+
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#define internalLED 2
+// LED Define
+#define internalLED 2 
+#define redLED 16 //GPIO 16 - D0
 
+// Wireless Define
 const char* wifiSSID = "APTRG-LAB";
 const char* wifiPassword = "gsglantaidua";
 
+// MQTT Define
 const char* mqttServerIP = "192.168.1.2";
 const int mqttPort = 1883;
 
-
+// delay milis
 int valueRandom = 0;
 int lastMsg = 0;
 // untuk pesan message
@@ -18,20 +65,157 @@ String hum_str;
 char temp[50];
 char hum[50];
 
-//MPU
-#include <Wire.h>
-const int MPU6050_addr=0x68;
-int16_t AccX,AccY,AccZ,Temp,GyroX,GyroY,GyroZ;
-
 WiFiClient myESP;
 PubSubClient client(myESP);
 
-void MPUSetup(){
+     //https://github.com/tzapu/WiFiManager
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+/* =========================================================================
+   NOTE: In addition to connection 5/3.3v, GND, SDA, and SCL, this sketch
+   depends on the MPU-6050's INT pin being connected to the ESP8266 GPIO15
+   pin.
+ * ========================================================================= */
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+#ifdef OUTPUT_READABLE_EULER
+float euler[3];         // [psi, theta, phi]    Euler angle container
+#endif
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+#endif
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+// quaternion components in a [w, x, y, z] format (not best for parsing
+// on a remote host such as Processing or something though)
+//#define OUTPUT_READABLE_QUATERNION
+
+// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+// (in degrees) calculated from the quaternions coming from the FIFO.
+// Note that Euler angles suffer from gimbal lock (for more info, see
+// http://en.wikipedia.org/wiki/Gimbal_lock)
+//#define OUTPUT_READABLE_EULER
+
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
+// components with gravity removed. This acceleration reference frame is
+// not compensated for orientation, so +X is always +X according to the
+// sensor, just without the effects of gravity. If you want acceleration
+// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
+//#define OUTPUT_READABLE_REALACCEL
+
+// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
+// components with gravity removed and adjusted for the world frame of
+// reference (yaw is relative to initial orientation, since no magnetometer
+// is present in this case). Could be quite handy in some cases.
+//#define OUTPUT_READABLE_WORLDACCEL
+
+// uncomment "OUTPUT_TEAPOT_OSC" if you want output that matches the
+// format used for the InvenSense teapot demo
+
+#define INTERRUPT_PIN 15 // use pin 15 on ESP8266
+
+
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void mpu_setup()
+{
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
-  Wire.beginTransmission(MPU6050_addr);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
 }
 
 void wifiSetup(){
@@ -40,42 +224,138 @@ void wifiSetup(){
     delay(500);
     Serial.println("Waiting, connection to Wifi..");
     Serial.print("SSID : "); Serial.println(wifiSSID);
+    
+    // give notification LED
+    redLEDHL(); 
   }
-  Serial.println("Connected to the WiFI Network"); 
+  Serial.println("Connected to the WiFI Network "); 
   Serial.print("Connected Network "); Serial.println(wifiSSID);
   Serial.print("IP Local "); Serial.println(WiFi.localIP());
 }
 
-void MPUMain(){
-  Wire.beginTransmission(MPU6050_addr);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU6050_addr,14,true);
-  AccX=Wire.read()<<8|Wire.read();
-  AccY=Wire.read()<<8|Wire.read();
-  AccZ=Wire.read()<<8|Wire.read();
-  Temp=Wire.read()<<8|Wire.read();
-  GyroX=Wire.read()<<8|Wire.read();
-  GyroY=Wire.read()<<8|Wire.read();
-  GyroZ=Wire.read()<<8|Wire.read();
-  Serial.print("AccX = "); Serial.print(AccX);
-  Serial.print(" || AccY = "); Serial.print(AccY);
-  Serial.print(" || AccZ = "); Serial.print(AccZ);
-  Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
-  Serial.print(" || GyroX = "); Serial.print(GyroX);
-  Serial.print(" || GyroY = "); Serial.print(GyroY);
-  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
-  delay(100);
-}
-void setup() {
-  // put your setup code here, to run once:
+void setup(void)
+{
   Serial.begin(57600);
-  wifiSetup();
-  MPUSetup();
-  //Initialize MQTT Connection
-  client.setServer(mqttServerIP, mqttPort);
-  client.setCallback(callback); // callback for incoming message
+   wifiSetup();
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+
+  //reset saved settings
+  //wifiManager.resetSettings();
+
+  //fetches ssid and pass from eeprom and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //and goes into a blocking loop awaiting configuration
+
+  Serial.print(F("WiFi connected! IP address: "));
+
+  mpu_setup();
   pinMode(internalLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+}
+
+void mpu_loop()
+{
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+
+  // wait for MPU interrupt or extra packet(s) available
+  if (!mpuInterrupt && fifoCount < packetSize) return;
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_QUATERNION
+    // display quaternion values in easy matrix form: w x y z
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    Serial.print("quat\t");
+    Serial.print(q.w);
+    Serial.print("\t");
+    Serial.print(q.x);
+    Serial.print("\t");
+    Serial.print(q.y);
+    Serial.print("\t");
+    Serial.println(q.z);
+#endif
+
+
+#ifdef OUTPUT_READABLE_EULER
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetEuler(euler, &q);
+    Serial.print("euler\t");
+    Serial.print(euler[0] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(euler[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(euler[2] * 180/M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180/M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_REALACCEL
+    // display real acceleration, adjusted to remove gravity
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    Serial.print("areal\t");
+    Serial.print(aaReal.x);
+    Serial.print("\t");
+    Serial.print(aaReal.y);
+    Serial.print("\t");
+    Serial.println(aaReal.z);
+#endif
+
+#ifdef OUTPUT_READABLE_WORLDACCEL
+    // display initial world-frame acceleration, adjusted to remove gravity
+    // and rotated based on known orientation from quaternion
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+    Serial.print("aworld\t");
+    Serial.print(aaWorld.x);
+    Serial.print("\t");
+    Serial.print(aaWorld.y);
+    Serial.print("\t");
+    Serial.println(aaWorld.z);
+#endif
+  }
 }
 
 void reconnect(){
@@ -83,6 +363,7 @@ void reconnect(){
   while(!client.connected()){
     Serial.println("Connecting to MQTT Server..");
     Serial.print("IP MQTT Server : "); Serial.println(mqttServerIP);
+    redLEDHL(); 
 
     bool hasConnection = client.connect("ZeroESP-1"); // connect(id,username,password) -> true if connect
     if(hasConnection){
@@ -95,7 +376,9 @@ void reconnect(){
     }
   }
   client.publish("esp/test", "Reconnecting");
-  client.subscribe("esp/test");
+  client.subscribe("esp/test");  
+  client.subscribe("esp/cmd");
+
 }
 
 void callback(char* topic, byte* payload, unsigned int length){
@@ -103,10 +386,25 @@ void callback(char* topic, byte* payload, unsigned int length){
   Serial.println("Message Arrived");
   Serial.print("Topic :"); Serial.println(topic);
   Serial.print("Message : ");
+  String pesan = "";
   for(int i=0; i < length; i++){
     Serial.print((char)payload[i]);
+
+    pesan += (char)payload[i];
+
   }
-  Serial.println();
+     if (topic == "esp/cmd") {
+       if(pesan == "ON" ){
+          Serial.println("LED ON.. Warning");
+          digitalWrite(redLED, HIGH);
+  
+       } else if(pesan == "OFF"){
+          Serial.println("LED OFF.. Safety");
+          digitalWrite(redLED,LOW);
+       }
+     }
+  Serial.print("Pesan masuk :");
+  Serial.println(pesan);
   Serial.println("--------");
 }
 
@@ -116,12 +414,25 @@ void notifLED(){
   digitalWrite(internalLED,LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void redLEDHL(){
+  digitalWrite(redLED, HIGH);
+  delay(100);
+  digitalWrite(redLED, LOW);
+}
 
-  if (!client.connected()){
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void)
+{
+   if (!client.connected()){
     reconnect();
   }
+    mpu_loop();
+
   valueRandom = random(10,100);
   client.loop(); //looping forever the client
   
@@ -139,7 +450,5 @@ void loop() {
     Serial.println(message);
     notifLED();
   }
-  MPUMain();
+
 }
-
-
